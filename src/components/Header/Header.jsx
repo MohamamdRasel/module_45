a3fca21112741775f353aa12fadfa2a00a96d13a
Mohammad <mohammadrasel201@gmail.com>
2023-10-27 09:23:29 +0600

    completed module 45

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
@@ -6,11 +6,12 @@ const Header = () => {
         <div>            
             <nav>
                 <p><span>My Website</span></p>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/posts">Posts</Link>
-                <Link to="/contact">Contact</Link>
-                <Link to="/users">Users</Link>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/about">About</NavLink>
+                <NavLink to="/posts">Posts</NavLink>
+                <NavLink to="/users">Users</NavLink>
+                <NavLink to="/contact">Contact</NavLink>
+                
                
             </nav>
         </div>
